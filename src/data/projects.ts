d288d64745abcc51f9172aa70d5e98b0c4a65e26
Pulkit Banta <pulkitbanta01@gmail.com>
2023-09-24 00:15:22 +0530

    feat(projects): add projects

---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: 'ReNews',
+    description:
+      'ReNews is built on top of Hacker news which fetches top 30 news for the day. By easily marking a news as favorite you can come back to it later.',
+    note: 'This was made over a weekend',
+    dayTime: 'May, 2020',
+    repoLink: 'https://github.com/PulkitBanta/renews',
+  },
+  {
+    title: 'Dev Blogg',
+    description:
+      'A full stack web application in which multiple users can write and share blogs with each other. It has three color modes (dark, light and reading) which helps in different scenarios.',
+    note: 'This was my first big project with Angular',
+    dayTime: 'Oct, 2020',
+    repoLink: 'https://github.com/PulkitBanta/dev-blogg',
+  },
+  {
+    title: 'React component template',
+    description:
+      'An extension created for vscode which helps you to create opinionated react components with styling (css-in-jsx), test and types file included.',
+    note: 'This helped my quickly develop and build projects in one of my company.',
+    dayTime: 'Jan, 2022',
+    repoLink: 'https://github.com/PulkitBanta/react-component-generator-vscode',
+  },
+  {
+    title: 'Fast weather',
+    description:
+      'A simple webapp for checking the weather in your area, input your city name and get the weather result in seconds.',
+    note: 'This was my first project with React.',
+    dayTime: 'Jan, 2021',
+    repoLink: 'https://github.com/PulkitBanta/fast-weather',
+  },
+];
