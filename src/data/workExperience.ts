29b0066058d257a2bf4173c114a6745e711d6d29
Pulkit Banta <pulkitbanta01@gmail.com>
2023-09-24 14:59:13 +0530

    feat: work exp layout

---
+++ src/data/workExperience.ts
@@ -0,0 +1,26 @@
+export const workExperience = [
+  {
+    company: 'FunnelStory',
+    shortDescription:
+      'ReNews is built on top of Hacker news which fetches top 30 news for the day. By easily marking a news as favorite you can come back to it later.',
+    list: ['Web app', 'website', 'docs', 'e2e setup'],
+    timeWithDuration: 'Oct, 2022 - Present',
+    role: 'Front-end Developer',
+  },
+  {
+    company: 'Medly',
+    shortDescription:
+      'A full stack web application in which multiple users can write and share blogs with each other. It has three color modes (dark, light and reading) which helps in different scenarios.',
+    list: ['Web app', 'website', 'docs', 'e2e setup'],
+    timeWithDuration: 'Apr 2021 - Oct 2022',
+    role: 'Front-end Developer',
+  },
+  {
+    company: 'Acquest Solutions',
+    shortDescription:
+      'A simple webapp for checking the weather in your area, input your city name and get the weather result in seconds.',
+    list: ['Web app', 'website', 'docs', 'e2e setup'],
+    timeWithDuration: 'Jan, 2021 - April-2021',
+    role: 'Front-end Intern',
+  },
+];
