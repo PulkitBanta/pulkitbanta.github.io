b0f8ac562da4b9dc6bcac09f8221c5cbe34c2270
Pulkit Banta <pulkitbanta01@gmail.com>
2024-01-01 12:49:51 +0530

    feat(data): update work experience section

--- src/data/workExperience.ts
+++ src/data/workExperience.ts
@@ -1,26 +1,43 @@
 export const workExperience = [
   {
     company: 'FunnelStory',
-    shortDescription:
-      'ReNews is built on top of Hacker news which fetches top 30 news for the day. By easily marking a news as favorite you can come back to it later.',
-    list: ['Web app', 'website', 'docs', 'e2e setup'],
+    shortDescription: 'Building UI for FunnelStory, go check out the website at https://funnelstory.ai',
+    list: ['Implementing <Cool /> features to help you understand your customers easily 😉'],
     timeWithDuration: 'Oct, 2022 - Present',
     role: 'Front-end Developer',
   },
   {
     company: 'Medly',
-    shortDescription:
-      'A full stack web application in which multiple users can write and share blogs with each other. It has three color modes (dark, light and reading) which helps in different scenarios.',
-    list: ['Web app', 'website', 'docs', 'e2e setup'],
+    shortDescription: 'Created Khora Healthcare app and maintained internal tools',
+    list: [
+      'Lead UI development of the product with a team of two and created MVP features within 8 months with unit testing',
+      'Contributed to open source react component library Medly-components',
+      'Implemented file upload and listing of uploaded files with AWS S3',
+      'Implemented Nested tabs layout to view information on a single page with route & query param state management so that sharing views is easy',
+    ],
     timeWithDuration: 'Apr 2021 - Oct 2022',
     role: 'Front-end Developer',
   },
   {
+    company: 'Medly',
+    shortDescription: 'Enhanced internal tools with various integrations',
+    list: [
+      'Implemented auth with GitHub Auth to onboard the developers easily',
+      'Implemented the integration of AWS Cloudwatch logs with a polling of 10 sec to show the result of deployment',
+      'Upgraded the UI and UX of the application making it easy to use',
+    ],
+    timeWithDuration: 'Apr 2021 - Oct 2022',
+    role: 'Intern Front-end Developer',
+  },
+  {
     company: 'Acquest Solutions',
-    shortDescription:
-      'A simple webapp for checking the weather in your area, input your city name and get the weather result in seconds.',
-    list: ['Web app', 'website', 'docs', 'e2e setup'],
+    shortDescription: 'Developed new features for a school management website while also improving the codebase',
+    list: [
+      'Lead a team of two developers on the UI and taught best practices',
+      'Created dynamic Angular forms which can be used to create an online assignment',
+      'Created modules for multiple assignments which can then be assigned to students as required',
+    ],
     timeWithDuration: 'Jan, 2021 - April-2021',
-    role: 'Front-end Intern',
+    role: 'Intern Front-end Developer',
   },
 ];
