6a105add1553934f14f4e60bc4715d77c5d07480
Pulkit Banta <pulkitbanta01@gmail.com>
2024-03-02 17:22:07 +0530

    build: add config, deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pulkitbanta.github.io',
+  base: '/',
   server: {
     port: 3001,
   },
