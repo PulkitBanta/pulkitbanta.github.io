2fd1bfe0d5b10112dcc887d64a9a97d9ef876a59
Pulkit Banta <pulkitbanta01@gmail.com>
2024-03-02 12:12:58 +0530

    feat: version 2 (#1)
    * feat: initialize astro
    
    * chore: setup eslint, prettier
    
    * feat(font): add poppins font
    
    * feat: add basic front-page
    
    * feat: add animation steps
    
    * feat(workflow): add build workflow
    
    * feat(icons): add icons, button hover effect
    
    * feat: wave animation, skew screen
    
    * feat(projects): add projects
    
    * feat: project card style, favicon
    
    * feat: basic work section layout
    
    * feat(projects): card style
    
    * feat: work exp layout
    
    * feat: connect text, remove email
    
    * feat: padding, site meta and image
    
    * feat(workCardList): add timeline view
    
    * feat(data): update work experience section
    
    * fix(font): use import
    
    * fix(projectCard): remove horizontal padding github button

---
+++ src/data/workExperience.ts
@@ -0,0 +1,43 @@
+export const workExperience = [
+  {
+    company: 'FunnelStory',
+    shortDescription: 'Building UI for FunnelStory, go check out the website at https://funnelstory.ai',
+    list: ['Implementing <Cool /> features to help you understand your customers easily 😉'],
+    timeWithDuration: 'Oct, 2022 - Present',
+    role: 'Front-end Developer',
+  },
+  {
+    company: 'Medly',
+    shortDescription: 'Created Khora Healthcare app and maintained internal tools',
+    list: [
+      'Lead UI development of the product with a team of two and created MVP features within 8 months with unit testing',
+      'Contributed to open source react component library Medly-components',
+      'Implemented file upload and listing of uploaded files with AWS S3',
+      'Implemented Nested tabs layout to view information on a single page with route & query param state management so that sharing views is easy',
+    ],
+    timeWithDuration: 'Apr 2021 - Oct 2022',
+    role: 'Front-end Developer',
+  },
+  {
+    company: 'Medly',
+    shortDescription: 'Enhanced internal tools with various integrations',
+    list: [
+      'Implemented auth with GitHub Auth to onboard the developers easily',
+      'Implemented the integration of AWS Cloudwatch logs with a polling of 10 sec to show the result of deployment',
+      'Upgraded the UI and UX of the application making it easy to use',
+    ],
+    timeWithDuration: 'Apr 2021 - Oct 2022',
+    role: 'Intern Front-end Developer',
+  },
+  {
+    company: 'Acquest Solutions',
+    shortDescription: 'Developed new features for a school management website while also improving the codebase',
+    list: [
+      'Lead a team of two developers on the UI and taught best practices',
+      'Created dynamic Angular forms which can be used to create an online assignment',
+      'Created modules for multiple assignments which can then be assigned to students as required',
+    ],
+    timeWithDuration: 'Jan, 2021 - April-2021',
+    role: 'Intern Front-end Developer',
+  },
+];
